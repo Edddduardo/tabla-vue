ccfbce071f53ec58cdca2e65f3da362139edada0
Edddduardo <173631@ids.upchiapas.edu.mx>
2019-01-27 22:27:34 -0600

    Bajo tarea

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
+import table from './components/table.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+new Vue({
+  render: h => h(table),
+}).$mount('#table')
+
